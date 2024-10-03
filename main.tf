dc9648a30583a82eed5f6a39b96a614e238b82a1
skaslam1776 <skaslam1776@gmail.com>
2024-10-04 00:56:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  region = "ap-south-1"
+region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
-  ami           = "ami-078264b8ba71bc45e"
-  instance_type = "t2.micro"
-  key_name      = "terraformkey"
-  vpc_security_group_ids = ["sg-02b20a5b329680509"]
-  tags = {
-    Name = "shakir01"
-  }
+ami = "ami-078264b8ba71bc45e"
+instance_type = "t2.micro"
+key_name = "terraformkey"
+vpc_security_group_ids = ["sg-02b20a5b329680509"]
+tags = {
+Name = "shakir01"
+}
 }
