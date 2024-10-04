919f7b5c7a1adda53d21fd258713df9285ba0371
skaslam1776 <skaslam1776@gmail.com>
2024-10-04 15:16:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,8 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-0d1622042e957c247"
   instance_type = "t2.micro"
+  key_name      = "terraformkey"
+  vpc_security_group_ids = ["sg-02b20a5b329680509"]
   tags = {
     Name = "shakir01"
   }
