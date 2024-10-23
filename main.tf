ee556659948ac5f9ef3b45cb82af61a433f4b177
Shakir Khan <183756036+skaslam1776@users.noreply.github.com>
2024-10-23 20:01:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,14 +4,14 @@ provider "aws" {
 
 resource "aws_instance" "one" {
   count = 4
-  ami = "ami-078264b8ba71bc45e"
-  instance_type = "t2.micro"
+  ami = "ami-0e0e417dfa2028266"
+  instance_type = "t2.medium"
   key_name      = "Terraform"
   vpc_security_group_ids = ["sg-0c7484c06d8541775"]
   tags = {
-    Name = var.instance_name
+    Name = var.instance_name[count.index]
   }
 }
 variable "instance_name" {
-default = ["jenkins","nexus","tomcat1","tomcat2"]
+default = ["jenkins", "nexus", "tomcat1", "tomcat2"]
 }
