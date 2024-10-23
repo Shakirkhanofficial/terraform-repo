9f67b5cdc62cc3cdbcd7869957b4acfad350ff5e
Shakir Khan <183756036+skaslam1776@users.noreply.github.com>
2024-10-23 20:15:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
   count = 4
   ami = "ami-0e0e417dfa2028266"
   instance_type = "t2.medium"
-  key_name      = "Terraform"
+  key_name      = "projectbydevops"
   vpc_security_group_ids = ["sg-0c7484c06d8541775"]
   tags = {
     Name = var.instance_name[count.index]
