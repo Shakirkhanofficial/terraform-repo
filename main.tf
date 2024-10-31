743c2766ddd95e79e64c148fbffd692a4b95f308
Shakir Khan <183756036+skaslam1776@users.noreply.github.com>
2024-10-31 23:52:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ provider "aws" {
 
 resource "aws_instance" "one" {
   count = 4
-  ami = "ami-0e0e417dfa2028266"
-  instance_type = "t2.medium"
+  ami = "ami-0ddc798b3f1a5117e "
+  instance_type = "t2.micro"
   key_name      = "DevopsProject"
-  vpc_security_group_ids = ["sg-0c7484c06d8541775"]
+  vpc_security_group_ids = ["sg-0970e3aea4b78ba88"]
   tags = {
     Name = var.instance_name[count.index]
   }
